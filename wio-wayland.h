1986411f2fff605825b2afe1bb757b04fc4c3c2f
Elaine Gibson <ypsvlq@gmail.com>
2024-09-25 17:00:53 +0100

    wayland fix

---
+++ wio-wayland.h
@@ -0,0 +1,3 @@
+#include <wayland-client-core.h>
+extern struct wl_proxy *(**wio_wl_proxy_marshal_flags)(struct wl_proxy *, uint32_t, const struct wl_interface *, uint32_t, uint32_t, ...);
+#define wl_proxy_marshal_flags (*wio_wl_proxy_marshal_flags)
