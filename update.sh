66f90a7a90f735c0dbc1f85cc69edc7b6b915319
Elaine Gibson <ypsvlq@gmail.com>
2026-05-30 20:13:01 +0100

    wayland pointer-gestures protocol

--- update.sh
+++ update.sh
@@ -24,6 +24,7 @@ protocols='
     unstable/pointer-constraints/pointer-constraints-unstable-v1
     unstable/relative-pointer/relative-pointer-unstable-v1
     unstable/text-input/text-input-unstable-v3
+    unstable/pointer-gestures/pointer-gestures-unstable-v1
 '
 urls='
     https://cvsweb.openbsd.org/checkout/src/include/sndio.h?rev=1.17
@@ -35,11 +36,11 @@ for header in $headers; do
     cp -r /usr/include/$header .
 done
 rm -r X11/dri X11/fonts GL/internal xkbcommon/xkbregistry.h
-for url in $urls; do
-    curl -OL $url
-done
 for protocol in ../wayland/wayland $protocols; do
     name=${protocol##*/}
     wayland-scanner client-header </usr/share/wayland-protocols/$protocol.xml >$name-client-protocol.h
     wayland-scanner private-code </usr/share/wayland-protocols/$protocol.xml >$name-protocol.c
+done
+for url in $urls; do
+    curl -OL $url
 done
